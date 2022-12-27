7c3637c70a60fca6eb588729f916ad4b536e8239
alexandrerodgomesnet <alexandrerodgomes@hotmail.com>
2022-12-27 16:27:20 -0300

    Adicionar arquivos de projeto.

---
+++ test/CursoOnline.Dominio.Test/Cursos/CursoTest.cs
@@ -0,0 +1,38 @@
+﻿using Xunit;
+
+namespace CursoOnline.Dominio.Test.Cursos
+{
+	public class CursoTest
+	{
+		[Fact]
+		public void DeveCriarCurso()
+		{
+			string nome = "Excel Avançado";
+			decimal cargaHoraria = 80;
+			string publicoAlvo = "Estudantes";
+			decimal valor = 620;
+			var curso = new Curso(nome, cargaHoraria, publicoAlvo, valor);
+
+			Assert.Equal(nome, curso.Nome);
+			Assert.Equal(cargaHoraria, curso.CargaHoraria);
+			Assert.Equal(publicoAlvo, curso.PublicoAlvo);
+			Assert.Equal(valor, curso.Valor);
+		}		
+	}
+
+	public class Curso
+	{
+		public Curso(string nome, decimal cargaHoraria, string publicoAlvo, decimal valor)
+		{
+			Nome = nome;
+			CargaHoraria = cargaHoraria;
+			PublicoAlvo = publicoAlvo;
+			Valor = valor;
+		}
+
+		public string Nome { get; private set; }
+		public decimal CargaHoraria { get; private set; }
+		public string PublicoAlvo { get; private set; }
+		public decimal Valor { get; private set; }
+	}
+}
