d235c82bec4f6f1761b48d196d11e6073e99bed1
alexandrerodgomesnet <alexandrerodgomes@hotmail.com>
2022-12-27 16:51:34 -0300

    Validando campo nome quando nulo ou vazio.

--- test/CursoOnline.Dominio.Test/Cursos/CursoTest.cs
+++ test/CursoOnline.Dominio.Test/Cursos/CursoTest.cs
@@ -1,4 +1,5 @@
 ﻿using ExpectedObjects;
+using System;
 using Xunit;
 
 namespace CursoOnline.Dominio.Test.Cursos
@@ -19,7 +20,37 @@ namespace CursoOnline.Dominio.Test.Cursos
 			var curso = new Curso(cursoEsperado.Nome, cursoEsperado.CargaHoraria, cursoEsperado.PublicoAlvo, cursoEsperado.Valor);
 
 			cursoEsperado.ToExpectedObject().ShouldMatch(curso);
-		}		
+		}
+
+		[Fact]
+		public void NomeDoCursoNaoPodeSerVazio()
+		{
+			var cursoEsperado = new
+			{
+				Nome = "Excel Avançado",
+				CargaHoraria = 80.00M,
+				PublicoAlvo = PublicoAlvo.Estudantes,
+				Valor = 620M
+			};
+
+			Assert.Throws<ArgumentException>(() => 
+				new Curso(string.Empty, cursoEsperado.CargaHoraria, cursoEsperado.PublicoAlvo, cursoEsperado.Valor));
+		}
+
+		[Fact]
+		public void NomeDoCursoNaoPodeSerNulo()
+		{
+			var cursoEsperado = new
+			{
+				Nome = "Excel Avançado",
+				CargaHoraria = 80.00M,
+				PublicoAlvo = PublicoAlvo.Estudantes,
+				Valor = 620M
+			};
+
+			Assert.Throws<ArgumentException>(() =>
+				new Curso(null, cursoEsperado.CargaHoraria, cursoEsperado.PublicoAlvo, cursoEsperado.Valor));
+		}
 	}
 
 	public enum PublicoAlvo
@@ -34,6 +65,9 @@ namespace CursoOnline.Dominio.Test.Cursos
 	{
 		public Curso(string nome, decimal cargaHoraria, PublicoAlvo publicoAlvo, decimal valor)
 		{
+			if (string.IsNullOrEmpty(nome))
+				throw new ArgumentException();
+
 			Nome = nome;
 			CargaHoraria = cargaHoraria;
 			PublicoAlvo = publicoAlvo;
